ba5af07151cb0b4fc91c8a09e04592d01ffe23ce
RhettSmithgall <rhettsmithgall@gmail.com>
2026-04-15 21:57:15 -0400

    minecart detection! a trained yolo model!

--- minecartDetection.py
+++ minecartDetection.py
@@ -2,6 +2,8 @@ import cv2
 import mss
 import numpy as np
 import math
+import os
+from ultralytics import YOLO
 
 def main():
     with mss.mss() as sct:
@@ -12,29 +14,34 @@ def main():
             "height": 850
         }
 
+        # make sure positives folder exists
+        os.makedirs("positives", exist_ok=True)
+
+        img_counter = 0
+
         while True:
             #capture the game screen
             screenshot = sct.grab(gameScreen)
             frame = np.array(screenshot)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
 
-            playerShot = frame[10:850,10:650]
+            playerShot = frame[10:850,200:500]
 
-            output = frame.copy()
+            trackshot = frame[100:900,400:1200]
+            
+            model = YOLO("runs/detect/train2/weights/best.pt")
 
-            fullscreen = findlines(frame,frame)
+            player_pos = detect_minecart(playerShot,model)
 
-            fullscreen = findKart(playerShot,frame)
+            tracks_pos = findlines(trackshot)
 
-            scoreShot = frame[10:150,10:150]
-            cv2.imshow("score", scoreShot)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-            return
+            barricade_pos = findBarricades(trackshot)
 
-            print(read_score(frame))
+            print(f"player: {player_pos} tracks: {len(tracks_pos)} barricade?: {barricade_pos}")
 
-            cv2.imshow("Region Capture", fullscreen)
+            #fullscreen = roboVision(player_pos,tracks_pos,barricade_pos, frame)
+
+            #cv2.imshow("Region Capture", fullscreen)
 
             if cv2.waitKey(1) & 0xFF == 27:  # press ESC to quit
                 break
@@ -42,89 +49,69 @@ def main():
     cv2.destroyAllWindows()
     return
 
-def read_score(frame):
-    # Capture a specific area (left, top, right, bottom)
-    reader = easyocr.Reader(['en'])
+#see what the robot sees
+def roboVision(player_pos, tracks_pos, barriacade_pos, frame):
+    
+    x1, y1, x2, y2 = player_pos
+    x1 +=200
+    x2 +=200
+    # draw box
+    cv2.rectangle(frame, (x1, y1), (x2, y2), (255,0,0), 2)
 
-    # Extract only numbers from the image
-    results = reader.readtext('screenshot.png', allowlist='0123456789')
+    # label
+    cv2.putText(frame, "minecart", (x1, y1-10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,0), 2)
+    
+    for line in tracks_pos:
+        x1, y1, x2, y2 = tracks_pos
+        cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    # Print detected numbers and their confidence scores
-    for (bbox, text, prob) in results:
-        print(f"Detected Number: {text} (Confidence: {prob:.4f})")
+    MPx, MPy = barriacade_pos
+    if(MPx != 0 and MPy != 0):
+        cv2.putText(frame,"Barrricade",(MPx, MPy - 10),cv2.FONT_HERSHEY_SIMPLEX,0.6,(255, 0, 0),2)
+        cv2.rectangle(frame, (MPx,MPy),(MPx+40,MPy+60),(255,0,0),2)
 
-def findKart(screenshot,frame):
+    return frame
+
+
+def detect_minecart(frame,model):
+    results = model(frame, max_det=1, verbose=False, stream=False)
+
+    for r in results:
+        for box in r.boxes.xyxy:
+            x1, y1, x2, y2 = map(int, box)
+            return x1, y1, x2, y2
+    return 0,0,0,0
+
+def findBarricades(frame):
     method = cv2.TM_SQDIFF_NORMED
 
     # Read the images from the file
-    small_image = cv2.imread('minecart.png')
-    large_image = screenshot
+    small_image = cv2.imread('barricade.png')
+    large_image = frame
 
     result = cv2.matchTemplate(large_image, small_image, method)
 
     # We want the minimum squared difference
     mn,_,mnLoc,_ = cv2.minMaxLoc(result)
 
+    confidence = 1 - mn  # convert to "higher is better"
+
+    if confidence < 0.7:
+        return 0,0
+
     # Draw the rectangle:
     # Extract the coordinates of our best match
     MPx,MPy = mnLoc
 
-    # Step 2: Get the size of the template. This is the same size as the match.
-    trows,tcols = small_image.shape[:2]
-
-    # Step 3: Draw the rectangle on large_image
-    #print(f"Detected position: x={MPx}, y={MPy}")
-    cv2.putText(
-    frame,
-    "minecart",
-    (MPx, MPy - 10),
-    cv2.FONT_HERSHEY_SIMPLEX,
-    0.6,
-    (0, 0, 255),
-    2)
-    cv2.rectangle(frame, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
-
-    # Display the original image with the rectangle around the match.
-    #cv2.imshow('output',fullscreen)
-
     # The image is only displayed if we call this
-    return frame
+    return MPx,MPy
 
 def get_angle(x1, y1, x2, y2):
     return abs(math.degrees(math.atan2(y2 - y1, x2 - x1)))
 
-def showCartMask(img):
-    cart_colors = [
-        (0x1e, 0x30, 0x44),
-        (0x17, 0x3d, 0x57),
-        (0x65, 0x7c, 0x8e),
-        (0x3f, 0x43, 0x56),
-        (0x34, 0x28, 0x35),
-        (0x32, 0x29, 0x36)
-    ]
-
-    tolerance = 25
-
-    mask = np.zeros(img.shape[:2], dtype=np.uint8)
-
-    for (b, g, r) in cart_colors:
-        lower = np.array([b - tolerance, g - tolerance, r - tolerance])
-        upper = np.array([b + tolerance, g + tolerance, r + tolerance])
-
-        color_mask = cv2.inRange(img, lower, upper)
-        mask = cv2.bitwise_or(mask, color_mask)
-
-    # apply mask
-    result = cv2.bitwise_and(img, img, mask=mask)
-
-    # show debug windows
-    cv2.imshow("mask", mask)
-    cv2.imshow("filtered", result)
-
-    return result
-
-def findlines(screenshot,frame):
-    img = screenshot
+def findlines(frame):
+    img = frame
     # --- 3 track colors (hex -> BGR) ---
     colors = [
         (0xA7, 0xD3, 0xE7),  # e7d3a7
@@ -162,6 +149,8 @@ def findlines(screenshot,frame):
         maxLineGap=10
     )
 
+    tracks = []
+
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line[0]
@@ -170,10 +159,8 @@ def findlines(screenshot,frame):
             
             if (length > 50) and (angle == 0 or angle == 45):
                 #print(angle)
-                cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                tracks.append((x1, y1, x2, y2))
 
-
-    return frame
-
+    return tracks
 
 main()
